7891b53074370bc38d10a51f1683b800a0d90ef9
elcachorroloko <raitoyagami2013@live.com>
2019-10-02 19:12:06 -0400

    Finalizado

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,22 @@
 const express = require('express');
-const routes = express.Router();
-const SessionController = require('./controllers/SessionController');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
 
-routes.post('/users', SessionController.store);
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
+routes.post('/sessions', SessionController.store);
+
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
